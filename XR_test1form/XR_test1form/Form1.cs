ac5cc1ca2a7185c3f7162e3e5a32dd221f1052a6
Sebmono <Sebmono@users.noreply.github.com>
2014-06-11 22:09:38 -0400

    trying to upload stats database

--- XR_test1form/XR_test1form/Form1.cs
+++ XR_test1form/XR_test1form/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Data.SqlClient;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
@@ -36,7 +37,7 @@ namespace XR_test1form
             xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
             xlWorkSheet.Cells[1, 1] = "http://csharp.net-informations.com";
 
-            xlWorkBook.SaveAs("csharp.net-informations.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            xlWorkBook.SaveAs(@"E:\GitHub\SimCode\csharp.net-informations.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
             xlWorkBook.Close(true, misValue, misValue);
             xlApp.Quit();
 
@@ -44,7 +45,7 @@ namespace XR_test1form
             releaseObject(xlWorkBook);
             releaseObject(xlApp);
 
-            MessageBox.Show("Excel file created , you can find the file c:\\csharp.net-informations.xls");
+            MessageBox.Show(@"Excel file created , you can find the file E:\GitHub\SimCode\csharp.net-informations.xls");
         }
 
         private void excel_Open(object sender, EventArgs e)
@@ -55,7 +56,7 @@ namespace XR_test1form
             object misValue = System.Reflection.Missing.Value;
 
             xlApp = new Excel.Application();
-            xlWorkBook = xlApp.Workbooks.Open("csharp.net-informations.xls", 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            xlWorkBook = xlApp.Workbooks.Open(@"E:\GitHub\SimCode\csharp.net-informations.xls", 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
             xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
 
             MessageBox.Show(xlWorkSheet.get_Range("A1", "A1").Value2.ToString());
@@ -75,12 +76,13 @@ namespace XR_test1form
             Excel.Worksheet xlWorkSheet;
             Excel.Range range;
 
-            string str;
+            var str = 0;
+            string str2;
             int rCnt = 0;
             int cCnt = 0;
 
             xlApp = new Excel.Application();
-            xlWorkBook = xlApp.Workbooks.Open("csharp.net-informations.xls", 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            xlWorkBook = xlApp.Workbooks.Open(@"E:\GitHub\SimCode\TeamStats.xlsx", 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
             xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
 
             range = xlWorkSheet.UsedRange;
@@ -89,8 +91,10 @@ namespace XR_test1form
             {
                 for (cCnt = 1; cCnt <= range.Columns.Count; cCnt++)
                 {
-                    str = (string)(range.Cells[rCnt, cCnt] as Excel.Range).Value2;
-                    MessageBox.Show(str);
+                    str = (range.Cells[rCnt, cCnt] as Excel.Range).Value2;
+                    str2 = str.ToString();
+                    MessageBox.Show(str2);
+                   //MessageBox.Show
                 }
             }
 
@@ -102,7 +106,95 @@ namespace XR_test1form
             releaseObject(xlApp);
         }
 
+        private void Read_OLEDB(object sender, EventArgs e)
+        {
+            DataGridView dataGridView1 = new DataGridView();
 
+            try
+            {
+                System.Data.OleDb.OleDbConnection MyConnection;
+                System.Data.DataSet DtSet;
+                System.Data.OleDb.OleDbDataAdapter MyCommand;
+                MyConnection = new System.Data.OleDb.OleDbConnection("provider=Microsoft.Jet.OLEDB.4.0;Data Source='E:\\GitHub\\SimCode\\csharp.net-informations.xls';Extended Properties=Excel 8.0;");
+                MyCommand = new System.Data.OleDb.OleDbDataAdapter("select * from [Sheet1$]", MyConnection);
+                MyCommand.TableMappings.Add("Table", "TestTable");
+                DtSet = new System.Data.DataSet();
+                MyCommand.Fill(DtSet);
+                dataGridView1.DataSource = DtSet.Tables[0];
+                MyConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void Insert_OLEDB(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Data.OleDb.OleDbConnection MyConnection;
+                System.Data.OleDb.OleDbCommand myCommand = new System.Data.OleDb.OleDbCommand();
+                string sql = null;
+                MyConnection = new System.Data.OleDb.OleDbConnection("provider=Microsoft.Jet.OLEDB.4.0;Data Source='E:\\GitHub\\SimCode\\csharp.net-informations.xls';Extended Properties=Excel 8.0;");
+                MyConnection.Open();
+                myCommand.Connection = MyConnection;
+                sql = "Insert into [Sheet1$] (id,name) values('5','e')";
+                myCommand.CommandText = sql;
+                myCommand.ExecuteNonQuery();
+                MyConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        /*private void excel_Export(object sender, EventArgs e)
+        {
+            SqlConnection cnn;
+            string connectionString = null;
+            string sql = null;
+            string data = null;
+            int i = 0;
+            int j = 0;
+
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+
+            xlApp = new Excel.ApplicationClass();
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            connectionString = "data source=servername;initial catalog=databasename;user id=username;password=password;";
+            cnn = new SqlConnection(connectionString);
+            cnn.Open();
+            sql = "SELECT * FROM Product";
+            SqlDataAdapter dscmd = new SqlDataAdapter(sql, cnn);
+            DataSet ds = new DataSet();
+            dscmd.Fill(ds);
+
+            for (i = 0; i <= ds.Tables[0].Rows.Count - 1; i++)
+            {
+                for (j = 0; j <= ds.Tables[0].Columns.Count - 1; j++)
+                {
+                    data = ds.Tables[0].Rows[i].ItemArray[j].ToString();
+                    xlWorkSheet.Cells[i + 1, j + 1] = data;
+                }
+            }
+
+            xlWorkBook.SaveAs("csharp.net-informations.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            releaseObject(xlWorkSheet);
+            releaseObject(xlWorkBook);
+            releaseObject(xlApp);
+
+            MessageBox.Show("Excel file created , you can find the file c:\\csharp.net-informations.xls");
+        }*/
 
         private void releaseObject(object obj)
         {
